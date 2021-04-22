0b5a2127d78914243381d05c3ff64ca4da8eb1a8
ewgeney <ewgeney@mail.ru>
2021-04-22 15:25:09 +0300

    update

--- app/src/main/java/com/example/my_movie/activity/MovieInfoActivity.java
+++ app/src/main/java/com/example/my_movie/activity/MovieInfoActivity.java
@@ -1,6 +1,7 @@
 package com.example.my_movie.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,8 +16,18 @@ import com.android.volley.toolbox.Volley;
 import com.example.my_movie.R;
 import com.squareup.picasso.Picasso;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
 
 public class MovieInfoActivity extends AppCompatActivity {
 
@@ -73,7 +84,8 @@ public class MovieInfoActivity extends AppCompatActivity {
                     String plot = response.getString("Plot");
                     String posterBigSizeUrl = response.getString("Poster");
 
-                    titleInfoTextView.setText(titleInfo);
+                    translate(titleInfo);
+
                     ratingTextView.setText(rating);
                     yearInfoTextView.setText(year);
                     countryTextView.setText(country);
@@ -98,4 +110,45 @@ public class MovieInfoActivity extends AppCompatActivity {
 
         requestQueue.add(jsonObjectRequest);
     }
+
+    private void translate(String s) {
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "text=" +s+ "&to=ru&from=en");
+        okhttp3.Request request = new okhttp3.Request.Builder()
+                .url("https://nlp-translation.p.rapidapi.com/v1/translate")
+                .post(body)
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .addHeader("x-rapidapi-key", "api key")
+                .addHeader("x-rapidapi-host", "nlp-translation.p.rapidapi.com")
+                .build();
+
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull okhttp3.Response response) throws IOException {
+
+                String serverAnswer = Objects.requireNonNull(response.body()).string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        updateUI(serverAnswer);
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+    });
+
+    }
+
+    private void updateUI(String serverAnswer) {
+        titleInfoTextView.setText(serverAnswer);
+        Log.wtf("serverAnswer", serverAnswer);
+    }
+
 }
